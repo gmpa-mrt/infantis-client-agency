18284d17e49a52c83d00a189a338b5391af8d0bd
gmpa-mrt <gaetanmeurant@gmail.com>
2023-09-07 10:11:07 +0200

    fix nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useAuth } from '../context/AuthContext.jsx'
 const Navbar = () => {
   const { logout } = useAuth()
   return (
-    <nav className={"w-full fixed top-0 flex justify-center items-center space-x-5 py-4 bg-opacity-50 backdrop-blur-md bg-white "}>
+    <nav className={"w-full fixed top-0 flex justify-center items-center space-x-5 py-4 bg-opacity-50 backdrop-blur-md bg-white z-50 "}>
       <Link className={"text-xl font-bond text-stone-900"} to="/home#library">Bibliothèque</Link>
       <Link className={"text-xl font-bond text-stone-900"} to="/home#account">Mon compte</Link>
       <button className={"text-xl font-bond text-stone-900"} onClick={logout}>Se déconnecter</button>
